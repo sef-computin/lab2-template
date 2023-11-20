2c2a44047f01cae6f07405f5a941e6fdf96b0159
sef-computin <inch.bdg@gmail.com>
2023-11-20 18:59:22 +0300

    bonus service

--- src/flight-service/main.go
+++ src/flight-service/main.go
@@ -40,7 +40,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "8060"
 	}
 
 	log.Println("Server is listening on port: ", port)
--- src/ticket-service/main.go
+++ src/ticket-service/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"lab2/src/flight-service/dbhandler"
-	"lab2/src/flight-service/handlers"
+	"lab2/src/ticket-service/dbhandler"
+	"lab2/src/ticket-service/handlers"
 	"log"
 	"net/http"
 	"os"
@@ -28,19 +28,18 @@ func main() {
 		panic(err)
 	}
 
-	flightHandler := &handlers.FlightHandler{
+	ticketHandler := &handlers.TicketHandler{
 		DBHandler: *dbhandler.InitDBHandler(db),
 	}
 
 	router := gin.Default()
 
-	router.GET("/api/v1/flights", flightHandler.GetAllFlightsHandler)
-	router.GET("api/v1/flight/:flightNumber", flightHandler.GetFlightHandler)
-	router.GET("api/v1/flight/airport/:airportId", flightHandler.GetAirportHandler)
+	router.POST("/api/v1/tickets", ticketHandler.BuyTicketHandler)
+	router.GET("api/v1/tickets/:username", ticketHandler.GetTicketsByUsernameHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "8070"
 	}
 
 	log.Println("Server is listening on port: ", port)
