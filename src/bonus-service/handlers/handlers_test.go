79826e0cdfa9f87d4b53bbd750af259195300d77
sef-computin <inch.bdg@gmail.com>
2023-11-21 23:29:38 +0300

    unit test

---
+++ src/bonus-service/handlers/handlers_test.go
@@ -0,0 +1,204 @@
+package handlers
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"lab2/src/bonus-service/dbhandler"
+	"lab2/src/bonus-service/models"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+
+	_ "github.com/lib/pq"
+)
+
+func SetUpRouter() *gin.Engine {
+	router := gin.Default()
+	return router
+}
+
+func TestCreatePrivilege(t *testing.T) {
+	dbURL := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+		"postgres", 5432, "postgres", "privileges", "postgres")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	bh := &BonusHandler{
+		DBHandler: *dbhandler.InitDBHandler(db),
+	}
+
+	privilege := &models.Privilege{
+		Username: "tuser1",
+		Balance:  0,
+	}
+
+	r := SetUpRouter()
+	r.POST("bonus/privilege", bh.CreatePrivilegeHandler)
+
+	jsonValue, _ := json.Marshal(privilege)
+	req, _ := http.NewRequest("POST", "/bonus/privilege", bytes.NewBuffer(jsonValue))
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestUpdatePrivilege(t *testing.T) {
+	dbURL := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+		"postgres", 5432, "postgres", "privileges", "postgres")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	bh := &BonusHandler{
+		DBHandler: *dbhandler.InitDBHandler(db),
+	}
+
+	privilege := &models.Privilege{
+		Username: "tuser1",
+		Balance:  0,
+	}
+
+	r := SetUpRouter()
+	r.POST("bonus/:username", bh.UpdatePrivilegeHandler)
+
+	jsonValue, _ := json.Marshal(privilege)
+	req, _ := http.NewRequest("POST", "/bonus/tuser1", bytes.NewBuffer(jsonValue))
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestGetPrivilegeByUsername(t *testing.T) {
+
+	dbURL := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+		"postgres", 5432, "postgres", "privileges", "postgres")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	bh := &BonusHandler{
+		DBHandler: *dbhandler.InitDBHandler(db),
+	}
+
+	r := SetUpRouter()
+	r.GET("bonus/:username", bh.GetPrivilegeByUsernameHandler)
+
+	req, _ := http.NewRequest("GET", "/bonus/tuser1", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	var privilege models.Privilege
+	json.Unmarshal(w.Body.Bytes(), &privilege)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.NotEmpty(t, privilege)
+
+	req, _ = http.NewRequest("GET", "/bonus/tuser99", nil)
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusNotFound, w.Code)
+}
+
+func TestCreatePrivilegeHistoryHandler(t *testing.T) {
+	dbURL := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+		"postgres", 5432, "postgres", "privileges", "postgres")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	bh := &BonusHandler{
+		DBHandler: *dbhandler.InitDBHandler(db),
+	}
+
+	record := &models.PrivilegeHistory{
+		PrivilegeID:   555,
+		TicketUID:     "tuid",
+		Date:          "12.12.2012",
+		BalanceDiff:   -150,
+		OperationType: "FILL",
+	}
+
+	r := SetUpRouter()
+	r.POST("/bonus", bh.CreatePrivilegeHistoryHandler)
+
+	jsonValue, _ := json.Marshal(record)
+	req, _ := http.NewRequest("POST", "/bonus", bytes.NewBuffer(jsonValue))
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+}
+
+func TestGetHistoryById(t *testing.T) {
+	dbURL := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+		"postgres", 5432, "postgres", "privileges", "postgres")
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	bh := &BonusHandler{
+		DBHandler: *dbhandler.InitDBHandler(db),
+	}
+
+	r := SetUpRouter()
+	r.GET("bonus/history/:privilegeId", bh.GetPrivilegeByUsernameHandler)
+
+	req, _ := http.NewRequest("GET", "bonus/history/555", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	var privilege models.Privilege
+	json.Unmarshal(w.Body.Bytes(), &privilege)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.NotEmpty(t, privilege)
+
+	req, _ = http.NewRequest("GET", "bonus/history/999", nil)
+	w = httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusNotFound, w.Code)
+}
---
+++ src/flight-service/handlers/handlers_test.go
@@ -0,0 +1,28 @@
+// package handlers
+
+// import (
+// 	"bytes"
+// 	"database/sql"
+// 	"encoding/json"
+// 	"fmt"
+// 	"lab2/src/flight-service/dbhandler"
+// 	"lab2/src/flight-service/models"
+// 	"net/http"
+// 	"net/http/httptest"
+// 	"testing"
+
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/stretchr/testify/assert"
+
+// 	_ "github.com/lib/pq"
+// )
+
+// func SetUpRouter() *gin.Engine {
+// 	router := gin.Default()
+// 	return router
+// }
+
+// func TestGetAllFlights(t *testing.T) {
+
+// }
+	
