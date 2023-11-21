509c7116718ea49a203afe367409e94ee08148a5
sef-computin <inch.bdg@gmail.com>
2023-11-21 23:49:41 +0300

    test

--- src/bonus-service/handlers/handlers_test.go
+++ src/bonus-service/handlers/handlers_test.go
@@ -1,178 +1,178 @@
 package handlers
 
-import (
-	"bytes"
-	"encoding/json"
-	"lab2/src/bonus-service/models"
-	"net/http"
-	"testing"
+// import (
+// 	"bytes"
+// 	"encoding/json"
+// 	"lab2/src/bonus-service/models"
+// 	"net/http"
+// 	"testing"
 
-	"github.com/gin-gonic/gin"
-	"github.com/stretchr/testify/assert"
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/stretchr/testify/assert"
 
-	_ "github.com/lib/pq"
-)
+// 	_ "github.com/lib/pq"
+// )
 
-func SetUpRouter() *gin.Engine {
-	router := gin.Default()
-	return router
-}
+// func SetUpRouter() *gin.Engine {
+// 	router := gin.Default()
+// 	return router
+// }
 
-func TestCreatePrivilege(t *testing.T) {
-	privilege := &models.Privilege{
-		Username: "tuser1",
-		Balance:  0,
-	}
+// func TestCreatePrivilege(t *testing.T) {
+// 	privilege := &models.Privilege{
+// 		Username: "tuser1",
+// 		Balance:  0,
+// 	}
 
-	jsonValue, _ := json.Marshal(privilege)
-	req, _ := http.NewRequest("POST", "localhost:8050/api/v1/bonus/privilege", bytes.NewBuffer(jsonValue))
+// 	jsonValue, _ := json.Marshal(privilege)
+// 	req, _ := http.NewRequest("POST", "localhost:8050/api/v1/bonus/privilege", bytes.NewBuffer(jsonValue))
 
-	assert.Equal(t, http.StatusOK, req.Response.Status)
-}
+// 	assert.Equal(t, http.StatusOK, req.Response.Status)
+// }
 
-func TestUpdatePrivilege(t *testing.T) {
-	// dbURL := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
-	// 	"postgres", 5432, "postgres", "privileges", "postgres")
+// func TestUpdatePrivilege(t *testing.T) {
+// 	// dbURL := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+// 	// 	"postgres", 5432, "postgres", "privileges", "postgres")
 
-	// db, err := sql.Open("postgres", dbURL)
-	// if err != nil {
-	// 	panic(err)
-	// }
+// 	// db, err := sql.Open("postgres", dbURL)
+// 	// if err != nil {
+// 	// 	panic(err)
+// 	// }
 
-	// err = db.Ping()
-	// if err != nil {
-	// 	panic(err)
-	// }
+// 	// err = db.Ping()
+// 	// if err != nil {
+// 	// 	panic(err)
+// 	// }
 
-	// bh := &BonusHandler{
-	// 	DBHandler: *dbhandler.InitDBHandler(db),
-	// }
+// 	// bh := &BonusHandler{
+// 	// 	DBHandler: *dbhandler.InitDBHandler(db),
+// 	// }
 
-	// privilege := &models.Privilege{
-	// 	Username: "tuser1",
-	// 	Balance:  0,
-	// }
+// 	// privilege := &models.Privilege{
+// 	// 	Username: "tuser1",
+// 	// 	Balance:  0,
+// 	// }
 
-	// r := SetUpRouter()
-	// r.POST("bonus/:username", bh.UpdatePrivilegeHandler)
+// 	// r := SetUpRouter()
+// 	// r.POST("bonus/:username", bh.UpdatePrivilegeHandler)
 
-	// jsonValue, _ := json.Marshal(privilege)
-	// req, _ := http.NewRequest("POST", "/bonus/tuser1", bytes.NewBuffer(jsonValue))
-	// w := httptest.NewRecorder()
-	// r.ServeHTTP(w, req)
+// 	// jsonValue, _ := json.Marshal(privilege)
+// 	// req, _ := http.NewRequest("POST", "/bonus/tuser1", bytes.NewBuffer(jsonValue))
+// 	// w := httptest.NewRecorder()
+// 	// r.ServeHTTP(w, req)
 
-	// assert.Equal(t, http.StatusOK, w.Code)
-}
+// 	// assert.Equal(t, http.StatusOK, w.Code)
+// }
 
-func TestGetPrivilegeByUsername(t *testing.T) {
+// func TestGetPrivilegeByUsername(t *testing.T) {
 
-	// dbURL := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
-	// 	"postgres", 5432, "postgres", "privileges", "postgres")
+// 	// dbURL := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+// 	// 	"postgres", 5432, "postgres", "privileges", "postgres")
 
-	// db, err := sql.Open("postgres", dbURL)
-	// if err != nil {
-	// 	panic(err)
-	// }
+// 	// db, err := sql.Open("postgres", dbURL)
+// 	// if err != nil {
+// 	// 	panic(err)
+// 	// }
 
-	// err = db.Ping()
-	// if err != nil {
-	// 	panic(err)
-	// }
+// 	// err = db.Ping()
+// 	// if err != nil {
+// 	// 	panic(err)
+// 	// }
 
-	// bh := &BonusHandler{
-	// 	DBHandler: *dbhandler.InitDBHandler(db),
-	// }
+// 	// bh := &BonusHandler{
+// 	// 	DBHandler: *dbhandler.InitDBHandler(db),
+// 	// }
 
-	// r := SetUpRouter()
-	// r.GET("bonus/:username", bh.GetPrivilegeByUsernameHandler)
+// 	// r := SetUpRouter()
+// 	// r.GET("bonus/:username", bh.GetPrivilegeByUsernameHandler)
 
-	// req, _ := http.NewRequest("GET", "/bonus/tuser1", nil)
-	// w := httptest.NewRecorder()
-	// r.ServeHTTP(w, req)
+// 	// req, _ := http.NewRequest("GET", "/bonus/tuser1", nil)
+// 	// w := httptest.NewRecorder()
+// 	// r.ServeHTTP(w, req)
 
-	// var privilege models.Privilege
-	// json.Unmarshal(w.Body.Bytes(), &privilege)
+// 	// var privilege models.Privilege
+// 	// json.Unmarshal(w.Body.Bytes(), &privilege)
 
-	// assert.Equal(t, http.StatusOK, w.Code)
-	// assert.NotEmpty(t, privilege)
+// 	// assert.Equal(t, http.StatusOK, w.Code)
+// 	// assert.NotEmpty(t, privilege)
 
-	// req, _ = http.NewRequest("GET", "/bonus/tuser99", nil)
-	// w = httptest.NewRecorder()
-	// r.ServeHTTP(w, req)
-	// assert.Equal(t, http.StatusNotFound, w.Code)
-}
+// 	// req, _ = http.NewRequest("GET", "/bonus/tuser99", nil)
+// 	// w = httptest.NewRecorder()
+// 	// r.ServeHTTP(w, req)
+// 	// assert.Equal(t, http.StatusNotFound, w.Code)
+// }
 
-func TestCreatePrivilegeHistoryHandler(t *testing.T) {
-	// dbURL := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
-	// 	"postgres", 5432, "postgres", "privileges", "postgres")
+// func TestCreatePrivilegeHistoryHandler(t *testing.T) {
+// 	// dbURL := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+// 	// 	"postgres", 5432, "postgres", "privileges", "postgres")
 
-	// db, err := sql.Open("postgres", dbURL)
-	// if err != nil {
-	// 	panic(err)
-	// }
+// 	// db, err := sql.Open("postgres", dbURL)
+// 	// if err != nil {
+// 	// 	panic(err)
+// 	// }
 
-	// err = db.Ping()
-	// if err != nil {
-	// 	panic(err)
-	// }
+// 	// err = db.Ping()
+// 	// if err != nil {
+// 	// 	panic(err)
+// 	// }
 
-	// bh := &BonusHandler{
-	// 	DBHandler: *dbhandler.InitDBHandler(db),
-	// }
+// 	// bh := &BonusHandler{
+// 	// 	DBHandler: *dbhandler.InitDBHandler(db),
+// 	// }
 
-	// record := &models.PrivilegeHistory{
-	// 	PrivilegeID:   555,
-	// 	TicketUID:     "tuid",
-	// 	Date:          "12.12.2012",
-	// 	BalanceDiff:   -150,
-	// 	OperationType: "FILL",
-	// }
+// 	// record := &models.PrivilegeHistory{
+// 	// 	PrivilegeID:   555,
+// 	// 	TicketUID:     "tuid",
+// 	// 	Date:          "12.12.2012",
+// 	// 	BalanceDiff:   -150,
+// 	// 	OperationType: "FILL",
+// 	// }
 
-	// r := SetUpRouter()
-	// r.POST("/bonus", bh.CreatePrivilegeHistoryHandler)
+// 	// r := SetUpRouter()
+// 	// r.POST("/bonus", bh.CreatePrivilegeHistoryHandler)
 
-	// jsonValue, _ := json.Marshal(record)
-	// req, _ := http.NewRequest("POST", "/bonus", bytes.NewBuffer(jsonValue))
-	// w := httptest.NewRecorder()
-	// r.ServeHTTP(w, req)
+// 	// jsonValue, _ := json.Marshal(record)
+// 	// req, _ := http.NewRequest("POST", "/bonus", bytes.NewBuffer(jsonValue))
+// 	// w := httptest.NewRecorder()
+// 	// r.ServeHTTP(w, req)
 
-	// assert.Equal(t, http.StatusOK, w.Code)
+// 	// assert.Equal(t, http.StatusOK, w.Code)
 
-}
+// }
 
-func TestGetHistoryById(t *testing.T) {
-	// dbURL := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
-	// 	"postgres", 5432, "postgres", "privileges", "postgres")
+// func TestGetHistoryById(t *testing.T) {
+// 	// dbURL := fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=disable",
+// 	// 	"postgres", 5432, "postgres", "privileges", "postgres")
 
-	// db, err := sql.Open("postgres", dbURL)
-	// if err != nil {
-	// 	panic(err)
-	// }
+// 	// db, err := sql.Open("postgres", dbURL)
+// 	// if err != nil {
+// 	// 	panic(err)
+// 	// }
 
-	// err = db.Ping()
-	// if err != nil {
-	// 	panic(err)
-	// }
+// 	// err = db.Ping()
+// 	// if err != nil {
+// 	// 	panic(err)
+// 	// }
 
-	// bh := &BonusHandler{
-	// 	DBHandler: *dbhandler.InitDBHandler(db),
-	// }
+// 	// bh := &BonusHandler{
+// 	// 	DBHandler: *dbhandler.InitDBHandler(db),
+// 	// }
 
-	// r := SetUpRouter()
-	// r.GET("bonus/history/:privilegeId", bh.GetPrivilegeByUsernameHandler)
+// 	// r := SetUpRouter()
+// 	// r.GET("bonus/history/:privilegeId", bh.GetPrivilegeByUsernameHandler)
 
-	// req, _ := http.NewRequest("GET", "bonus/history/555", nil)
-	// w := httptest.NewRecorder()
-	// r.ServeHTTP(w, req)
+// 	// req, _ := http.NewRequest("GET", "bonus/history/555", nil)
+// 	// w := httptest.NewRecorder()
+// 	// r.ServeHTTP(w, req)
 
-	// var privilege models.Privilege
-	// json.Unmarshal(w.Body.Bytes(), &privilege)
+// 	// var privilege models.Privilege
+// 	// json.Unmarshal(w.Body.Bytes(), &privilege)
 
-	// assert.Equal(t, http.StatusOK, w.Code)
-	// assert.NotEmpty(t, privilege)
+// 	// assert.Equal(t, http.StatusOK, w.Code)
+// 	// assert.NotEmpty(t, privilege)
 
-	// req, _ = http.NewRequest("GET", "bonus/history/999", nil)
-	// w = httptest.NewRecorder()
-	// r.ServeHTTP(w, req)
-	// assert.Equal(t, http.StatusNotFound, w.Code)
-}
+// 	// req, _ = http.NewRequest("GET", "bonus/history/999", nil)
+// 	// w = httptest.NewRecorder()
+// 	// r.ServeHTTP(w, req)
+// 	// assert.Equal(t, http.StatusNotFound, w.Code)
+// }
